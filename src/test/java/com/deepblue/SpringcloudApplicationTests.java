c1f20d3e675fc93a4d88699c13c5424f30b6523a
yangjianwei <2017585616@qq.com>
2022-02-24 16:28:45 +0800

    first commit

---
+++ src/test/java/com/deepblue/SpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepblue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

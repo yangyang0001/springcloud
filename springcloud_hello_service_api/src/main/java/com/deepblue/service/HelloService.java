3395963390725d742d560214bc5a150a419d7ae4
yangjianwei <2017585616@qq.com>
2024-11-27 21:29:33 +0900

    修改springcloud 相关的配置

--- springcloud_hello_service_api/src/main/java/com/deepblue/service/HelloService.java
+++ springcloud_hello_service_api/src/main/java/com/deepblue/service/HelloService.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * @FeignClient 注解中的 value 必须为 当前服务名称, 不区分大小写!
+ */
 @FeignClient(value = "springcloud-hello-service", fallback = HelloServiceFallback.class)
 public interface HelloService {
 
--- springcloud_ribbon_consumer/src/main/java/com/deepblue/command/Client.java
+++ springcloud_ribbon_consumer/src/main/java/com/deepblue/command/Client.java
@@ -7,9 +7,9 @@ public class Client {
 
     public static void main(String[] args) {
         Receiver receiver = new Receiver();
-        Command  command  = new ConcreteCommand(receiver);
+        Command command = new ConcreteCommand(receiver);
 
-        Invoker  invoker  = new Invoker();
+        Invoker invoker = new Invoker();
         invoker.setCommand(command);
 
         invoker.inaction();
